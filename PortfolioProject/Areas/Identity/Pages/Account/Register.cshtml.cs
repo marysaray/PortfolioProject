14f3b6d798514f68df891dfa440c3a13f0ea2a67
marysaray <82470838+marysaray@users.noreply.github.com>
2022-10-04 23:18:44 -0700

    Added username section to register form.

--- PortfolioProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PortfolioProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -80,6 +80,12 @@ namespace PortfolioProject.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             /// <summary>
+            /// Add Username section to Registration
+            /// </summary>
+            [Required]
+            public string Username { get; set; }
+
+            /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
@@ -114,7 +120,7 @@ namespace PortfolioProject.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.Username, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
