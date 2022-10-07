0d1b2a43ab785469aae7d3f44f54fdc8f702db9f
Mary Saray (Sah-Rye) <82470838+marysaray@users.noreply.github.com>
2022-10-07 13:35:53 -0700

    Mary issue#6 add username to login (#34)
    * Login uses Username to sign in.
    
    * Added summary to changes for Username model

--- PortfolioProject/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ PortfolioProject/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -63,10 +63,10 @@ namespace PortfolioProject.Areas.Identity.Pages.Account
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
+            ///     Modify code to change behavior from Email to Username
             /// </summary>
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            public string Username { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -111,7 +111,7 @@ namespace PortfolioProject.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
