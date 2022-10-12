4c9c8bbc1b0e68368eb278290b9e9d0dd219911a
marysaray <82470838+marysaray@users.noreply.github.com>
2022-10-11 22:32:03 -0700

    Created a class for Identity role support functionality.

---
+++ PortfolioProject/Models/IdentityHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PortfolioProject.Models
+{
+#nullable disable
+    /// <summary>
+    /// This class helps with role support for Identity.
+    /// </summary>
+    public static class IdentityHelper
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+
+        public static async Task CreateRoles(IServiceProvider provider, params string[] roles)
+        {
+            RoleManager<IdentityRole> roleManager = provider.GetService<RoleManager<IdentityRole>>();
+
+            foreach (string role in roles)
+            {
+                bool doesRoleExist = await roleManager.RoleExistsAsync(role);
+                if (!doesRoleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
