d3a5fa67bf921bc2c3d95ed323bfa51d4aa89851
marysaray <82470838+marysaray@users.noreply.github.com>
2022-10-16 15:35:36 -0700

    Added migration and updated database.

---
+++ PortfolioProject/Data/Migrations/20221016223152_AddedEventType.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioProject.Data.Migrations
+{
+    public partial class AddedEventType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.EventId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
--- PortfolioProject/Models/EventType.cs
+++ PortfolioProject/Models/EventType.cs
@@ -1,7 +1,12 @@
-﻿namespace PortfolioProject.Models
+﻿using MessagePack;
+using System.ComponentModel.DataAnnotations;
+using KeyAttribute = System.ComponentModel.DataAnnotations.KeyAttribute;
+
+namespace PortfolioProject.Models
 {
     public class EventType
     {
+        [Key]
         public int EventId { get; set; }
 
         public string Category { get; set; }
