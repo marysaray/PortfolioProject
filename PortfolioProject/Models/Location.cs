ede5c0b8e4b3bed8efa9079b23d4f82f7fb14fb4
marysaray <82470838+marysaray@users.noreply.github.com>
2022-10-17 14:30:26 -0700

    Created Location class.

---
+++ PortfolioProject/Models/Location.cs
@@ -0,0 +1,22 @@
+﻿using MessagePack;
+using System.ComponentModel.DataAnnotations;
+using KeyAttribute = System.ComponentModel.DataAnnotations.KeyAttribute;
+
+namespace PortfolioProject.Models
+{
+    public class Location
+    {
+        [Key]
+        public int LocationId { get; set; }
+
+        public string LocationName { get; set; }
+
+        public string  StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Zip { get; set; }
+    }
+}
