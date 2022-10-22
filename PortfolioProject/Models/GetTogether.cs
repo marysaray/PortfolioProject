9b0d87173ec83c5698a5db4a4604ce6ce204656e
marysaray <82470838+marysaray@users.noreply.github.com>
2022-10-22 15:08:18 -0700

    Created  a model for Get-Togethers.

---
+++ PortfolioProject/Models/GetTogether.cs
@@ -0,0 +1,14 @@
+﻿namespace PortfolioProject.Models
+{
+#nullable disable
+    public class GetTogether
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Place { get; set; }
+
+        public DateTime Date {get; set;}
+    }
+}
