cf9b7f4f71ad441b19911b600907cc868e2bfaf4
marysaray <82470838+marysaray@users.noreply.github.com>
2022-10-22 14:41:36 -0700

    Created controller for Wedding.

---
+++ PortfolioProject/Controllers/WeddingsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PortfolioProject.Data;
+using PortfolioProject.Models;
+
+namespace PortfolioProject.Controllers
+{
+    public class WeddingsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WeddingsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Weddings
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Love.ToListAsync());
+        }
+
+        // GET: Weddings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Love == null)
+            {
+                return NotFound();
+            }
+
+            var wedding = await _context.Love
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (wedding == null)
+            {
+                return NotFound();
+            }
+
+            return View(wedding);
+        }
+
+        // GET: Weddings/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Weddings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Date")] Wedding wedding)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(wedding);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(wedding);
+        }
+
+        // GET: Weddings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Love == null)
+            {
+                return NotFound();
+            }
+
+            var wedding = await _context.Love.FindAsync(id);
+            if (wedding == null)
+            {
+                return NotFound();
+            }
+            return View(wedding);
+        }
+
+        // POST: Weddings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Date")] Wedding wedding)
+        {
+            if (id != wedding.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(wedding);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WeddingExists(wedding.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(wedding);
+        }
+
+        // GET: Weddings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Love == null)
+            {
+                return NotFound();
+            }
+
+            var wedding = await _context.Love
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (wedding == null)
+            {
+                return NotFound();
+            }
+
+            return View(wedding);
+        }
+
+        // POST: Weddings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Love == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Love'  is null.");
+            }
+            var wedding = await _context.Love.FindAsync(id);
+            if (wedding != null)
+            {
+                _context.Love.Remove(wedding);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WeddingExists(int id)
+        {
+          return _context.Love.Any(e => e.Id == id);
+        }
+    }
+}
