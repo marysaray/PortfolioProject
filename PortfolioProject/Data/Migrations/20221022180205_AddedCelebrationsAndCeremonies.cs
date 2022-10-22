25a98ee3be4fa5ac9a908e2d901d5a72a9c0e4a7
marysaray <82470838+marysaray@users.noreply.github.com>
2022-10-22 11:16:46 -0700

    Added to Db context, migrated and updated database.

---
+++ PortfolioProject/Data/Migrations/20221022180205_AddedCelebrationsAndCeremonies.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioProject.Data.Migrations
+{
+    public partial class AddedCelebrationsAndCeremonies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CelebrationsAndCeremonies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CelebrationsAndCeremonies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CelebrationsAndCeremonies");
+        }
+    }
+}
