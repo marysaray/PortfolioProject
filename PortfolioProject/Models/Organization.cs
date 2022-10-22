b6b81cf6ae0dd3dc3e2dc5c9631c9a6206d9fa12
marysaray <82470838+marysaray@users.noreply.github.com>
2022-10-22 13:01:07 -0700

    Created a model for Organizations.

---
+++ PortfolioProject/Models/Organization.cs
@@ -0,0 +1,14 @@
+﻿namespace PortfolioProject.Models
+{
+#nullable disable
+    public class Organization
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Place { get; set; }
+    }
+}
