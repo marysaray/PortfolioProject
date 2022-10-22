530b0255c5ad75af65baf2a5dcad9a75d47ab0e6
marysaray <82470838+marysaray@users.noreply.github.com>
2022-10-22 13:53:30 -0700

    Created a model for Baby & Kids.

---
+++ PortfolioProject/Models/BabyAndKids.cs
@@ -0,0 +1,11 @@
+﻿namespace PortfolioProject.Models
+{
+#nullable disable
+    public class BabyAndKids
+    {
+        public int Id { get; set; } 
+
+        public string Name { get; set; }    
+
+    }
+}
