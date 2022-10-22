043bed34a43a037a1a3a0b13ff7b934d8616090f
marysaray <82470838+marysaray@users.noreply.github.com>
2022-10-21 21:30:08 -0700

    Created a model for Holidays.

---
+++ PortfolioProject/Models/Holidays.cs
@@ -0,0 +1,10 @@
+﻿namespace PortfolioProject.Models
+{
+#nullable disable
+    public class Holidays
+    {
+        public int Id { get; set; }
+
+        public string HolidayName { get; set; }
+    }
+}
