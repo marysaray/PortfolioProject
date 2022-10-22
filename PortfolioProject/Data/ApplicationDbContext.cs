ff85c0180435d037d23b45f88a6f6a1d9193de97
Mary Saray (Sah-Rye) <82470838+marysaray@users.noreply.github.com>
2022-10-21 21:41:29 -0700

    Mary issue#15 holiday view (#45)
    * Created a model for Holidays.
    
    * Added to Db context, migrated and updated database.
    
    * Created controller for Holidays.
    
    * Scaffolded for CRUD functionality.
    
    * Added Link to the main Layout Navbar.

--- PortfolioProject/Data/ApplicationDbContext.cs
+++ PortfolioProject/Data/ApplicationDbContext.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using PortfolioProject.Models;
 
+#nullable disable
+
 namespace PortfolioProject.Data
 {
     public class ApplicationDbContext : IdentityDbContext
@@ -14,5 +16,7 @@ namespace PortfolioProject.Data
         public DbSet<EventType> Categories { get; set; }
 
         public DbSet<Location> Locations { get; set; }
+
+        public DbSet<Holidays> Holidays { get; set; }
     }
 }
---
+++ PortfolioProject/Data/Migrations/20221022041259_AddedHolidays.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioProject.Data.Migrations
+{
+    public partial class AddedHolidays : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Holidays",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HolidayName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Holidays", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Holidays");
+        }
+    }
+}
