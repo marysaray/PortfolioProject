0f763a30d62381ee1b06d598f80d079e06215d21
Mary Saray (Sah-Rye) <82470838+marysaray@users.noreply.github.com>
2022-10-22 13:11:25 -0700

    Mary issue#18 organizations view (#48)
    * Created a model for Organizations.
    
    * Added to Db context, migrated and updated database.
    
    * Created controller for Organizations.
    
    * Scaffolded for CRUD functionality.
    
    * Added link to the main layout navbar.

---
+++ PortfolioProject/Data/Migrations/20221022195445_AddedOrganizations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioProject.Data.Migrations
+{
+    public partial class AddedOrganizations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Organizations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Place = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizations", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Organizations");
+        }
+    }
+}
