7273aa65e19ed28aabbba43b4f1e2319e3ff069f
marysaray <82470838+marysaray@users.noreply.github.com>
2022-10-28 15:07:37 -0700

    Created model for Event Forms.

---
+++ PortfolioProject/Models/EventForm.cs
@@ -0,0 +1,22 @@
+﻿using MessagePack;
+using System.ComponentModel.DataAnnotations;
+using KeyAttribute = System.ComponentModel.DataAnnotations.KeyAttribute;
+
+namespace PortfolioProject.Models
+{
+#nullable disable
+    public class EventForm
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name ="Event Title")]
+        public string EventTitle { get; set; }
+
+        public ContactInfo EventBy { get; set; }
+
+        public EventType Category { get; set; }
+
+        public Location Location { get; set; }
+    }
+}
