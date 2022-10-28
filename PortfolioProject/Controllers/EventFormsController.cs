f3b61da10a7b9ce375f11b2e2ffd7bc1887c8483
Mary Saray (Sah-Rye) <82470838+marysaray@users.noreply.github.com>
2022-10-28 15:29:02 -0700

    Mary issue#13 create from event (#55)
    * Created model for Event Forms.
    
    * Added to Db context, migrated and updated database.
    
    * Created controller for Event Forms.
    
    * Scaffolded for CRUD functionality.
    
    * Added link to the main layout navbar.
    
    * Authorized access only to contacts.

--- PortfolioProject/Controllers/EventFormsController.cs
+++ PortfolioProject/Controllers/EventFormsController.cs
@@ -12,85 +12,85 @@ using PortfolioProject.Models;
 namespace PortfolioProject.Controllers
 {
     [Authorize]
-    public class ContactsController : Controller
+    public class EventFormsController : Controller
     {
         private readonly ApplicationDbContext _context;
 
-        public ContactsController(ApplicationDbContext context)
+        public EventFormsController(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        // GET: Contacts
+        // GET: EventForms
         public async Task<IActionResult> Index()
         {
-              return View(await _context.Contacts.ToListAsync());
+              return View(await _context.EventForms.ToListAsync());
         }
 
-        // GET: Contacts/Details/5
+        // GET: EventForms/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || _context.Contacts == null)
+            if (id == null || _context.EventForms == null)
             {
                 return NotFound();
             }
 
-            var contactInfo = await _context.Contacts
+            var eventForm = await _context.EventForms
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (contactInfo == null)
+            if (eventForm == null)
             {
                 return NotFound();
             }
 
-            return View(contactInfo);
+            return View(eventForm);
         }
 
-        // GET: Contacts/Create
+        // GET: EventForms/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Contacts/Create
+        // POST: EventForms/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,PhoneNumber,Email")] ContactInfo contactInfo)
+        public async Task<IActionResult> Create([Bind("Id,EventTitle")] EventForm eventForm)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(contactInfo);
+                _context.Add(eventForm);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(contactInfo);
+            return View(eventForm);
         }
 
-        // GET: Contacts/Edit/5
+        // GET: EventForms/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.Contacts == null)
+            if (id == null || _context.EventForms == null)
             {
                 return NotFound();
             }
 
-            var contactInfo = await _context.Contacts.FindAsync(id);
-            if (contactInfo == null)
+            var eventForm = await _context.EventForms.FindAsync(id);
+            if (eventForm == null)
             {
                 return NotFound();
             }
-            return View(contactInfo);
+            return View(eventForm);
         }
 
-        // POST: Contacts/Edit/5
+        // POST: EventForms/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,PhoneNumber,Email")] ContactInfo contactInfo)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,EventTitle")] EventForm eventForm)
         {
-            if (id != contactInfo.Id)
+            if (id != eventForm.Id)
             {
                 return NotFound();
             }
@@ -99,12 +99,12 @@ namespace PortfolioProject.Controllers
             {
                 try
                 {
-                    _context.Update(contactInfo);
+                    _context.Update(eventForm);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!ContactInfoExists(contactInfo.Id))
+                    if (!EventFormExists(eventForm.Id))
                     {
                         return NotFound();
                     }
@@ -115,49 +115,49 @@ namespace PortfolioProject.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(contactInfo);
+            return View(eventForm);
         }
 
-        // GET: Contacts/Delete/5
+        // GET: EventForms/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.Contacts == null)
+            if (id == null || _context.EventForms == null)
             {
                 return NotFound();
             }
 
-            var contactInfo = await _context.Contacts
+            var eventForm = await _context.EventForms
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (contactInfo == null)
+            if (eventForm == null)
             {
                 return NotFound();
             }
 
-            return View(contactInfo);
+            return View(eventForm);
         }
 
-        // POST: Contacts/Delete/5
+        // POST: EventForms/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Contacts == null)
+            if (_context.EventForms == null)
             {
-                return Problem("Entity set 'ApplicationDbContext.Contacts'  is null.");
+                return Problem("Entity set 'ApplicationDbContext.EventForms'  is null.");
             }
-            var contactInfo = await _context.Contacts.FindAsync(id);
-            if (contactInfo != null)
+            var eventForm = await _context.EventForms.FindAsync(id);
+            if (eventForm != null)
             {
-                _context.Contacts.Remove(contactInfo);
+                _context.EventForms.Remove(eventForm);
             }
             
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool ContactInfoExists(int id)
+        private bool EventFormExists(int id)
         {
-          return _context.Contacts.Any(e => e.Id == id);
+          return _context.EventForms.Any(e => e.Id == id);
         }
     }
 }
---
+++ PortfolioProject/Data/Migrations/20221028215016_AddedEventForm.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioProject.Data.Migrations
+{
+    public partial class AddedEventForm : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EventForms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EventById = table.Column<int>(type: "int", nullable: true),
+                    CategoryEventId = table.Column<int>(type: "int", nullable: true),
+                    LocationId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventForms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EventForms_Categories_CategoryEventId",
+                        column: x => x.CategoryEventId,
+                        principalTable: "Categories",
+                        principalColumn: "EventId");
+                    table.ForeignKey(
+                        name: "FK_EventForms_Contacts_EventById",
+                        column: x => x.EventById,
+                        principalTable: "Contacts",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_EventForms_Locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Locations",
+                        principalColumn: "LocationId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventForms_CategoryEventId",
+                table: "EventForms",
+                column: "CategoryEventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventForms_EventById",
+                table: "EventForms",
+                column: "EventById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventForms_LocationId",
+                table: "EventForms",
+                column: "LocationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventForms");
+        }
+    }
+}
