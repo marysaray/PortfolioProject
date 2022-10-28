403da9ae5a6e50acd6a93cdee23ac21f75c9add6
marysaray <82470838+marysaray@users.noreply.github.com>
2022-10-28 14:30:29 -0700

    Created model for Contact Information.

---
+++ PortfolioProject/Models/ContactInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioProject.Models
+{
+#nullable disable
+    public class ContactInfo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
