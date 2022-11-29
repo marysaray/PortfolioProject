8a5cbb143573e8f41defefad5c0a4643c28b8b19
marysaray <82470838+marysaray@users.noreply.github.com>
2022-11-28 20:15:22 -0800

    Integrated migration and updated database.

---
+++ PortfolioProject/Data/Migrations/20221129040038_AddedGreetingForm.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioProject.Data.Migrations
+{
+    public partial class AddedGreetingForm : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GreetingForms",
+                columns: table => new
+                {
+                    GreetingId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GreetingForms", x => x.GreetingId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GreetingForms");
+        }
+    }
+}
