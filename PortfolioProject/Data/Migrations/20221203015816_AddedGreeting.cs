3266eed040cf24473151b94841dcc9559f218788
marysaray <82470838+marysaray@users.noreply.github.com>
2022-12-02 18:22:15 -0800

    Added migration and updated database.

---
+++ PortfolioProject/Data/Migrations/20221203015816_AddedGreeting.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioProject.Data.Migrations
+{
+    public partial class AddedGreeting : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GreetingTypeGreetingId",
+                table: "GreetingForms",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GreetingForms_GreetingTypeGreetingId",
+                table: "GreetingForms",
+                column: "GreetingTypeGreetingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GreetingForms_GreetingTypes_GreetingTypeGreetingId",
+                table: "GreetingForms",
+                column: "GreetingTypeGreetingId",
+                principalTable: "GreetingTypes",
+                principalColumn: "GreetingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GreetingForms_GreetingTypes_GreetingTypeGreetingId",
+                table: "GreetingForms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GreetingForms_GreetingTypeGreetingId",
+                table: "GreetingForms");
+
+            migrationBuilder.DropColumn(
+                name: "GreetingTypeGreetingId",
+                table: "GreetingForms");
+        }
+    }
+}
