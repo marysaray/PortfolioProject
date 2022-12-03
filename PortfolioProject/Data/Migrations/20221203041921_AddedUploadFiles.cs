89f444163a0902a0b0637c0d6686ec6675aa9ae8
marysaray <82470838+marysaray@users.noreply.github.com>
2022-12-02 20:23:48 -0800

    Added migration and updated database.

---
+++ PortfolioProject/Data/Migrations/20221203041921_AddedUploadFiles.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioProject.Data.Migrations
+{
+    public partial class AddedUploadFiles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                table: "GreetingForms",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                table: "GreetingForms");
+        }
+    }
+}
