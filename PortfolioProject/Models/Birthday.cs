965b646eb83203f091c691754efd8883c5af00ad
marysaray <82470838+marysaray@users.noreply.github.com>
2022-12-09 17:57:54 -0800

    Added documentation to Birthday class.

--- PortfolioProject/Models/Birthday.cs
+++ PortfolioProject/Models/Birthday.cs
@@ -1,12 +1,24 @@
 ﻿namespace PortfolioProject.Models
 {
 #nullable disable
+    /// <summary>
+    /// Represents a birthday event.
+    /// </summary>
     public class Birthday
     {
+        /// <summary>
+        /// The associated id to a birthday.
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// The name of the person's birthday.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// The date of the birthday.
+        /// </summary>
         public DateTime Date { get; set; }
     }
 }
