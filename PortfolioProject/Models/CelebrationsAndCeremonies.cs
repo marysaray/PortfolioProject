2a39768dc82d0bd1495ac4cbe6adf7a62e277947
marysaray <82470838+marysaray@users.noreply.github.com>
2022-12-09 18:00:20 -0800

    Added documentation to Celebrations and Ceremonies class.

--- PortfolioProject/Models/CelebrationsAndCeremonies.cs
+++ PortfolioProject/Models/CelebrationsAndCeremonies.cs
@@ -3,10 +3,19 @@
 namespace PortfolioProject.Models
 {
 #nullable disable
+    /// <summary>
+    /// Represents a celebration or ceremony.
+    /// </summary>
     public class CelebrationsAndCeremonies
     {
+        /// <summary>
+        /// The associated id to the event.
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// The name of the event.
+        /// </summary>
         [Display(Name = "Event Name")]
         public string EventName { get; set; }
     }
