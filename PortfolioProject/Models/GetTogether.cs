913db6490c42f1e1cf0e66b05b32438251dee232
marysaray <82470838+marysaray@users.noreply.github.com>
2022-12-09 18:03:33 -0800

    Added documentation to Get-Together class.

--- PortfolioProject/Models/GetTogether.cs
+++ PortfolioProject/Models/GetTogether.cs
@@ -1,14 +1,29 @@
 ﻿namespace PortfolioProject.Models
 {
 #nullable disable
+    /// <summary>
+    /// Represents a get-together event.
+    /// </summary>
     public class GetTogether
     {
+        /// <summary>
+        /// The associated id to the get-together.
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// The title of the get-together.
+        /// </summary>
         public string Title { get; set; }
 
+        /// <summary>
+        /// The get-together location.
+        /// </summary>
         public string Place { get; set; }
 
+        /// <summary>
+        /// The date and time of the get-together.
+        /// </summary>
         public DateTime Date {get; set;}
     }
 }
