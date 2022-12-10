6840079e16b4f83491ec087b496761460f9e3d38
marysaray <82470838+marysaray@users.noreply.github.com>
2022-12-09 18:17:32 -0800

    Added documentation to Wedding class.

--- PortfolioProject/Models/Wedding.cs
+++ PortfolioProject/Models/Wedding.cs
@@ -1,11 +1,25 @@
 ﻿namespace PortfolioProject.Models
 {
 #nullable disable
+    /// <summary>
+    /// Represents occasions of different events.
+    /// Engagement, Bridal, Groom, Rehearsal, etc..
+    /// </summary>
     public class Wedding
     {
+        /// <summary>
+        /// The associated id to the specific event.
+        /// </summary>
         public int Id { get; set; } 
 
+        /// <summary>
+        /// A short description of the event.
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// The date and time of the event.
+        /// </summary>
         public DateTime Date { get; set; }
     }
 }
