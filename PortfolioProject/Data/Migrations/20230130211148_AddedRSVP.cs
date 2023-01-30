de078172b9918c44d089ff70c62abdcaf7b9063a
marysaray <82470838+marysaray@users.noreply.github.com>
2023-01-30 13:24:18 -0800

    Added migration and updated database.

---
+++ PortfolioProject/Data/Migrations/20230130211148_AddedRSVP.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioProject.Data.Migrations
+{
+    public partial class AddedRSVP : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RSVPForms",
+                columns: table => new
+                {
+                    RSVPId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConfirmedEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RSVPResponse = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateSubmitted = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EventId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RSVPForms", x => x.RSVPId);
+                    table.ForeignKey(
+                        name: "FK_RSVPForms_EventForms_EventId",
+                        column: x => x.EventId,
+                        principalTable: "EventForms",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RSVPForms_EventId",
+                table: "RSVPForms",
+                column: "EventId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RSVPForms");
+        }
+    }
+}
