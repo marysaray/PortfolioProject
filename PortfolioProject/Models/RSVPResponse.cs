9f37b571f39fcedcc20a0419db1b271b673d7fbf
marysaray <82470838+marysaray@users.noreply.github.com>
2023-02-05 17:38:08 -0800

    Created RSVPResponseType class.

---
+++ PortfolioProject/Models/RSVPResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioProject.Models
+{
+#nullable disable
+    public class RSVPResponse
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string RSVPType { get; set; }
+    }
+}
